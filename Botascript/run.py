29e5056e98ddfe61edaa5bd11a4b6531364930b1
JulienElkaim <julienelk@gmail.com>
2019-05-30 16:14:20 +0200

    Order and Orders classes are functional; Tasks to be done can be retrieved; Logs can be updated

--- Botascript/run.py
+++ Botascript/run.py
@@ -1,5 +1,12 @@
-from Interface import *
+import Interface
 
 
-for a in getOrders():
-	print(a)
+orders = Interface.Orders()
+for order in orders:
+	print(order)
+
+print("========= AFTER calling .taff() method")
+
+orders.taff()
+for order in orders:
+	print(order)
