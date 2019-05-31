43dba089474345b6f25739b425c8e4b1f03bb0e0
JulienElkaim <julienelk@gmail.com>
2019-05-31 23:40:01 +0200

    Switched OFF /.bundle in gitignore to see if it impacts

---
+++ Botaserver/app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def home
+  	puts "================== OK MESSAGE HERE =================="
+  end
+end
--- Botaserver/config/routes.rb
+++ Botaserver/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  #get 'users/home'
+  get 'home', to: 'users#home', as: :home
+  root to: 'users#home'
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
