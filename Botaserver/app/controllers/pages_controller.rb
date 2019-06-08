96051bb59cf912de2763c1f4d2853d7135036498
JulienElkaim <julienelk@gmail.com>
2019-06-08 11:59:46 +0200

    Raw display of logs

--- Botaserver/app/controllers/pages_controller.rb
+++ Botaserver/app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 	skip_before_action :authenticate_user!, only: :autre
   def dashboard
+  	@logs = current_user.logs
   end
 
   def autre
--- Botaserver/app/helpers/pages_helper.rb
+++ Botaserver/app/helpers/pages_helper.rb
@@ -1,2 +1,22 @@
 module PagesHelper
+	def prefix_of_log(myLog)
+		
+		if myLog.message.include? "WARNING::"
+			return "WARNING"
+		elsif myLog.message.include? "SUCCESS::"
+			return "SUCCESS"
+		elsif myLog.message.include? "FAIL::"
+			return "FAIL"
+		end
+	end
+
+	def unprefix_this_log (myLog)
+		return myLog
+		.gsub("SUCCESS::","").to_s
+		.gsub("SUCCESS:: ","").to_s
+		.gsub("WARNING::","").to_s
+		.gsub("WARNING:: ","").to_s
+		.gsub("FAIL::","").to_s
+		.gsub("FAIL:: ","").to_s
+	end
 end
