e39d85414e572625446c44feb9e8d211f1d9244e
JulienElkaim <julienelk@gmail.com>
2019-06-08 22:47:50 +0200

    Form creation and update of accounts

--- Botaserver/app/controllers/accounts_controller.rb
+++ Botaserver/app/controllers/accounts_controller.rb
@@ -1,2 +1,43 @@
 class AccountsController < ApplicationController
+	
+	def new
+		@account = Account.new
+	end
+
+	def edit
+		pre_account = Account.find(params[:id]) 
+		if pre_account.user_id != current_user.id
+			flash[:alert] = "Vous tentez d'accéder à un compte ne vous appartenant pas."
+			redirect_to "/accounts"
+		end
+		@account = (current_user.id == pre_account.user_id) ? pre_account : Account.new
+	end
+
+	def create
+		tmp_params = account_params
+		tmp_params["user_id"] = current_user.id
+		tmp_params["network_id"] = 3
+
+		@account = Account.new(tmp_params)
+		@account.save
+
+		if !@account.id
+			puts "loooooool"
+			flash[:alert] = "Vous possédez déjà un compte sur ce réseau !"
+			redirect_to "/accounts"
+		else
+			redirect_to account_path(@account)
+		end
+		# Will raise ActiveModel::ForbiddenAttributesError
+	end
+
+	def update
+		@account = Account.find(params[:id])
+		@account.update(params[:account]) unless current_user.id != @account.user_id
+		# Will raise ActiveModel::ForbiddenAttributesError
+	end
+
+	def account_params
+    params.require(:account).permit(:network_login, :network_password)
+  end
 end
