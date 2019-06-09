87efff390b27890a8250666194557001a9de6a2e
JulienElkaim <julienelk@gmail.com>
2019-06-09 15:54:59 +0200

    Orders delete finished

--- Botaserver/app/controllers/orders_controller.rb
+++ Botaserver/app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
 
 	def index
 		#TO DO : Afficher tous les ordres que je possède
-		@orders = current_user.orders.sort_by{|order| order.exectime}
+		@orders = current_user.orders.where(alive: true).sort_by{|order| order.exectime}
 
 	end
 
@@ -28,10 +28,6 @@ class OrdersController < ApplicationController
 		end
 		# Will raise ActiveModel::ForbiddenAttributesError
 	end
-
-
-
-
 
 
 	def edit
@@ -65,37 +61,17 @@ class OrdersController < ApplicationController
 	def show
 	end
 
-	# def create
-	# 	tmp_params = account_params
-	# 	tmp_params["user_id"] = current_user.id
-		
-		
+	def destroy
+		@order = Order.find(params[:id])
+		if @order.user_id == current_user.id 
+			@order.update(alive: false)
+		else
+			flash[:alert] = "C'est pas bien du tout de vouloir supprimer les ordres des autres."
+		end
 
-	# 	@account = Account.new(tmp_params)
-	# 	@account.save
+		redirect_to orders_path
+	end
 
-	# 	if !@account.id
-	
-	# 		flash[:alert] = "Vous possédez déjà un compte sur #{Network.find(tmp_params["network_id"]).network_name} !"
-	# 		redirect_to "/accounts"
-	# 	else
-	# 		redirect_to accounts_path
-	# 	end
-	# 	# Will raise ActiveModel::ForbiddenAttributesError
-	# end
-
-	# def update
-	# 	@account = Account.find(params[:id])
-		
-	# 	if current_user.id != @account.user_id
-	# 		flash[:alert] = "Vous ne possédez pas ce compte #{@account.network.network_name}."
-			
-	# 	else
-	# 		@account.update(account_params)
-	# 	end
-	# 	redirect_to accounts_path
-	# 	# Will raise ActiveModel::ForbiddenAttributesError
-	# end
 
 	def order_params
     	params.require(:order).permit(:title, :network_id, :intervalle, :exectime, :endtime, :order_tag, :order_args)
